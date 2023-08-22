54fdb92f10191c5a26d0b3371c3692f5e7a27b73
VitoriaRossi <vitoria.rossi555@gmail.com>
2023-08-22 15:42:45 -0300

    Adiciona a resposta do exercício

---
+++ Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Dia de primavera");
+Console.WriteLine("Os pardais no jardim");
+Console.WriteLine("Tomam banho de areia.");
